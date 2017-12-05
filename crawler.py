b2e211b498f45c3edf9ebb363d0146bd26555288
yomancool <hyc404@nyu.edu>
2017-12-05 17:33:25 -0500

    parse 50 playlist each category

--- crawler.py
+++ crawler.py
@@ -1,26 +1,175 @@
 import requests
 from requests.auth import HTTPDigestAuth
 import json
+import base64
+import csv
 
-# Replace with the correct URL
-url = "https://api.spotify.com/v1/audio-features/06AKEBrKUckW0KREUWRnvT"
-myAuth = "Bearer BQAqPV8UWHcFW9F3gLR5rBOeSNxy9nYUO9_OZXvfskcECy7qZStS-1fx7UfAJet7lGCoz8rvnQa7ZXdN1me-O-VDOhYAFuJdZ_0NTzPwf3weKumdl9XN-E4BQQXooR2iYj05tLhlHxIK43dX"
-# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
-myResponse = requests.get(url,auth=myAuth verify=True)
-#print (myResponse.status_code)
+def getToken():
+	clientId = '6bbdf1b0b07b4c6bba857e2cc937074c'
+	clientSecret = 'de1acfa4836449b2afa6a874a441ad49'
 
-# For successful API call, response code will be 200 (OK)
-if(myResponse.ok):
+	encodeClient = base64.b64encode(bytes(clientId+':'+clientSecret, 'ascii')).decode('ascii')
+	encodeClient = 'Basic ' + encodeClient
+	tokenUrl = 'https://accounts.spotify.com/api/token'
+	tokenHeaders = {'Authorization': encodeClient}
+	tokenParams = {'grant_type': 'client_credentials'}
+	token = ''
+	# tokenResponse = requests.post(tokenUrl, params=tokenParams, headers=tokenHeaders, )
 
-    # Loading the response data into a dict variable
-    # json.loads takes in only binary or string variables so using content to fetch binary content
-    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-    jData = json.loads(myResponse.content)
+	tokenResponse=requests.post(tokenUrl, data=tokenParams, auth = (clientId, clientSecret))
 
-    print("The response contains {0} properties".format(len(jData)))
-    print("\n")
-    for key in jData:
-        print key + " : " + jData[key]
-else:
-  # If response code is not ok (200), print the resulting http error code with description
-    myResponse.raise_for_status()
+	if(tokenResponse.ok):
+		jData = json.loads(tokenResponse.content)
+		token = jData['access_token']
+		return token
+	else:
+		tokenResponse.raise_for_status()
+
+
+def getCategories(token):
+	# token = getToken()
+	url = 'https://api.spotify.com/v1/browse/categories'
+	headers = {'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token}
+
+	params = {'limit': '50'}
+
+	categories = []
+	myResponse = requests.get(url, data = params, headers=headers)
+	if(myResponse.ok):
+	    jData = json.loads(myResponse.content)
+	    tmpList = jData['categories']
+	    for item in tmpList['items']:
+	    	# tmp = item
+	    	categories.append(item['id'])
+	    # writeCsv("123.csv", jData)
+	    return categories
+
+	else:
+	  # If response code is not ok (200), print the resulting http error code with description
+	    myResponse.raise_for_status()
+
+def getPlayListFromCategory(id, token):
+	# token = getToken()
+	url = 'https://api.spotify.com/v1/browse/categories/'+id+'/playlists'
+	headers = {'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token}
+	params = {'limit': '50'}
+
+	hrefs = []
+	myResponse = requests.get(url, params=params, headers=headers)
+
+	if(myResponse.ok):
+	    jData = json.loads(myResponse.content)
+	    tmp = jData['playlists']
+	    for item in tmp['items']:
+	    	tmp = item['tracks']
+	    	href = tmp['href']
+	    	hrefs.append(href)
+	    return hrefs
+	else:
+	  # If response code is not ok (200), print the resulting http error code with description
+	    myResponse.raise_for_status()
+
+def getTracksFromPlayList(url, token):
+	# token = getToken()
+	# url = 'https://api.spotify.com/v1/users/11120963440/playlists/'+id+'/tracks'
+	# url = 'https://api.spotify.com/v1/users/spotify/playlists/37i9dQZF1DWWwaxRea1LWS/tracks'
+	headers = {'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token}
+
+	tracks = []
+	myResponse = requests.get(url, headers=headers)
+	if(myResponse.ok):
+	    jData = json.loads(myResponse.content)
+	    tmp = jData['items']
+	    for item in jData['items']:
+	    	info = item['track']
+	    	tracks.append(info['id'])
+	    # writeCsv("123.csv", jData)
+	    return tracks
+
+	else:
+	  # If response code is not ok (200), print the resulting http error code with description
+	    myResponse.raise_for_status()
+
+def writeCsv(newFileName, items, keys):
+	# print("write: "+id)
+	print(newFileName)
+	with open(newFileName, 'w', encoding='utf-8') as f:
+		writer = csv.writer(f)
+		writer.writerow(keys)
+		for track in items:
+			keys = []
+			if track is not None:
+				for key in track:
+					keys.append(key)
+				vals = []
+				for key in keys:
+					vals.append(track[key])
+				try:
+					writer.writerow(vals)
+				except:
+					print("out of range")
+	f.close()
+
+def getFeature(ids, token, i):
+	# token = getToken()
+
+	url = "https://api.spotify.com/v1/audio-features/?ids="
+	for id in ids:
+		if id is not None:
+			url += id + ','
+
+	headers = {'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token}
+
+	myResponse = requests.get(url, headers=headers)
+
+	if(myResponse.ok):
+	    jData = json.loads(myResponse.content)
+	    items = jData['audio_features']
+	    keys = []
+	    for track in items:
+	    	if track is not None:
+	    		for key in track:
+	    			keys.append(key)
+	    		writeCsv('feature/'+id+'.csv', items, keys)
+	    		break
+	else:
+	  # If response code is not ok (200), print the resulting http error code with description
+	    myResponse.raise_for_status()
+
+
+
+if __name__ == '__main__':
+	token = getToken()
+	categoryIds = getCategories(token)
+
+	#get playlists from catefory id	
+	playLists = []
+	for id in categoryIds:
+		playLists.append(getPlayListFromCategory(id, token))
+		print(len(playLists))
+
+	#get tracks from playlists
+	tracks = []
+	for i in range(len(playLists)):
+	# for i in range(15):
+		# if i < 12:
+		# 	continue
+		for url in playLists[i]:
+			tracks.append(getTracksFromPlayList(url, token))
+			print(len(tracks))
+	
+	#get audio feature from max 100 tracks in a batch
+	for i in range(len(tracks)):
+		ids = []
+
+		for id in tracks[i]:
+			ids.append(id)
+			if len(ids) == 100:
+				getFeature(ids, token, i)
+				ids = []
+				
+		getFeature(ids, token, i)
